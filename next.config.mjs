a29275c2d775837f836c1963c266bac3b8dfde77
Akriti Ghosh <akritighosh98@gmail.com>
2024-11-20 11:18:10 +0530

    Timeline configured

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https", // Protocol is "https"
+        hostname: "assets.aceternity.com", // Exact hostname (do not use wildcards like `**/`)
+        port: "", // Leave port empty for default ports
+        pathname: "/**/**", 
+      },
+    ],
+  },
+};
 
 export default nextConfig;
