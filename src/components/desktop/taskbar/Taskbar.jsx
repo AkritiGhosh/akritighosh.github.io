1ce8b9f390854b9f9e335745336716039e0c102a
Akriti Ghosh <akritighosh98@gmail.com>
2025-01-06 22:39:49 +0530

    Create Taskbar.jsx

---
+++ src/components/desktop/taskbar/Taskbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Taskbar = () => {
+  return (
+    <div className="w-full flex fixed h-12 bottom-0 bg-[#1a1a1a] items-center gap-5"></div>
+  );
+};
+
+export default Taskbar;
