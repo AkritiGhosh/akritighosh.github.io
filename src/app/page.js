f61c1b0b0169e516c42bcafcab032beeceec23b4
AkritiGhosh <akritighosh98@gmail.com>
2025-02-11 00:55:10 +0530

    Added modal for experience

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Modal from "@/components/window/Modal";
 import StartMenu from "@/components/desktop/StartMenu";
 import { ModalContext } from "@/context/ModelContext";
 import { useContext } from "react";
+import WorkExperience from "@/components/desktop/work-history/page";
 
 export default function Home() {
   const { bgURL } = useTheme();
@@ -15,14 +16,25 @@ export default function Home() {
         backgroundImage: "url(" + bgURL + ")",
         transition: "background-image 3s",
       }}
-      className="relative w-screen h-screen flex items-center bg-cover bg-center"
+      className="relative w-screen h-screen flex items-center bg-cover bg-center overflow-hidden"
     >
       {modals.START && (
         <Modal
           showTitleBar={false}
-          className={"absolute bottom-16 w-1/2 min-h-[50%] left-1/4 ring-white ring-1 ring-opacity-10 rounded"}
+          className={
+            "absolute bottom-16 w-1/2 min-h-[50%] left-1/4 ring-white ring-1 ring-opacity-10 rounded"
+          }
         >
           <StartMenu />
+        </Modal>
+      )}
+      {modals.EXPERIENCE && (
+        <Modal
+          showTitleBar={true}
+          title="Work History"
+          windowKey={"EXPERIENCE"}
+        >
+          <WorkExperience />
         </Modal>
       )}
     </div>
