8a887bb8d7836c08b6badab062b19cd0c6c5cfca
AkritiGhosh <akritighosh98@gmail.com>
2025-02-10 22:09:22 +0530

    code refactored

--- src/hooks/useTheme.jsx
+++ src/hooks/useTheme.jsx
@@ -5,30 +5,32 @@ import { useEffect, useMemo, useState } from "react";
 const useTheme = () => {
   const [photo, setPhoto] = useState(1);
   const [theme, setTheme] = useState("morning");
+ 
+  const updateTheme = () => {
+    const hour = new Date().getHours();
+    const body = document.body;
+    body.classList.remove(
+      "theme-morning",
+      "theme-afternoon",
+      "theme-evening",
+      "theme-night"
+    );
+
+    if (hour >= 4 && hour < 12) setTheme("morning");
+    else if (hour >= 12 && hour < 17) setTheme("afternoon");
+    else if (hour >= 17 && hour < 20) setTheme("evening");
+    else setTheme("night");
+
+    setPhoto((prev) => ((prev + 1) % 6) + 1);
+    body.classList.add(`theme-${theme}`);
+  };
+
   useEffect(() => {
-    const updateTheme = () => {
-      const hour = new Date().getHours();
-      const body = document.body;
-      body.classList.remove(
-        "theme-morning",
-        "theme-afternoon",
-        "theme-evening",
-        "theme-night"
-      );
-
-      if (hour >= 4 && hour < 12) setTheme("morning");
-      else if (hour >= 12 && hour < 17) setTheme("afternoon");
-      else if (hour >= 17 && hour < 20) setTheme("evening");
-      else setTheme("night");
-
-      setPhoto((prev) => ((prev + 1) % 6) + 1);
-      body.classList.add(`theme-${theme}`);
-    };
-
     updateTheme();
     const interval = setInterval(updateTheme, 30000); // Check every minute
     return () => clearInterval(interval); // Cleanup on unmount
   }, []);
+  
   const bgURL = useMemo(() => {
     return "/desktopBg/" + theme + "/" + photo + ".jpg";
   }, [theme, photo]);
