44d0b80a9308e4f362806d494380862bdbec5a24
AkritiGhosh <akritighosh98@gmail.com>
2025-02-11 17:33:16 +0530

    icons removed

--- src/components/desktop/taskbar/Taskbar.jsx
+++ src/components/desktop/taskbar/Taskbar.jsx
@@ -74,21 +74,7 @@ const Taskbar = () => {
         </button>
       </div>
       {isMobile && (
-        <div className="w-full h-8 flex items-center justify-around bg-[#181818]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5 8.25 12l7.5-7.5"
-            />
-          </svg>
+        <div className="w-full h-8 flex items-center justify-center bg-[#181818]">
           <button onClick={closeAllModals}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -105,7 +91,6 @@ const Taskbar = () => {
               />
             </svg>
           </button>
-          <span className="size-4" />
         </div>
       )}
     </div>
