a2eeb9d905901b0b6d8169b9f665a4844dbed4a3
AkritiGhosh <akritighosh98@gmail.com>
2025-02-11 14:50:58 +0530

    background changed

--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -24,7 +24,7 @@ export const Timeline = ({ data }) => {
 
   return (
     <div
-      className="w-full dark:bg-[#202020]/90 backdrop-blur-2xl font-sans md:px-10"
+      className="w-full dark:bg-[#202020] font-sans md:px-10"
       ref={containerRef}
     >
       <div className="max-w-7xl mx-auto pb-5 pt-10 md:pt-16 px-8 md:px-8 lg:px-10 text-center md:text-left">
@@ -39,7 +39,7 @@ export const Timeline = ({ data }) => {
             className="flex justify-start pt-10 md:pt-40 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-background flex items-center justify-center">
+              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-[#202020] flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-teal border border-neutral-300 dark:border-white p-2" />
               </div>
               <h3 className="hidden md:inline-block text-xl md:pl-20 md:text-5xl !leading-normal font-bold text-white font-nums">
