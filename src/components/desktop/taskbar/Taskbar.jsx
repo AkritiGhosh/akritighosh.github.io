7adad3de0ad7a92e169b430e74e9c2df8e9fdbef
AkritiGhosh <akritighosh98@gmail.com>
2025-02-11 14:50:47 +0530

    mobile view taskbar

--- src/components/desktop/taskbar/Taskbar.jsx
+++ src/components/desktop/taskbar/Taskbar.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import React, { useContext, useEffect, useState } from "react";
 
 const Taskbar = () => {
-  const { toggleModal } = useContext(ModalContext);
+  const { toggleModal, closeAllModals } = useContext(ModalContext);
   const [isMobile, setIsMobile] = useState(false);
 
   //choose the screen size
@@ -23,10 +23,10 @@ const Taskbar = () => {
 
   return (
     <div className="w-full flex fixed h-auto lg:h-14 bottom-0 flex-col items-center justify-center lg:gap-x-5 lg:border-t lg:border-stone-700">
-      <div className="w-full max-w-sm lg:max-w-full mx-auto  h-14 flex items-center justify-between lg:justify-center lg:gap-x-8 bg-transparent lg:bg-stone-950/90">
+      <div className="w-full max-w-sm lg:max-w-full mx-auto h-14 flex items-center justify-evenly lg:justify-center lg:gap-x-8 bg-transparent lg:bg-stone-950/90">
         <button
           onClick={() => toggleModal("START")}
-          className="cursor-pointer w-10 h-10 flex items-center justify-center bg-transparent p-2 rounded-sm hover:bg-stone-700/60"
+          className={`cursor-pointer w-10 h-10 flex items-center justify-center bg-black/70 lg:bg-transparent p-2 rounded-sm hover:bg-stone-700/60`}
         >
           <Image
             className="object-contain w-auto"
@@ -38,7 +38,7 @@ const Taskbar = () => {
         </button>
         <button
           onClick={() => toggleModal("EXPERIENCE")}
-          className="cursor-pointer w-10 h-10 flex items-center justify-center bg-transparent p-1.5 rounded-sm hover:bg-stone-700/60"
+          className={`cursor-pointer w-10 h-10 flex items-center justify-center bg-black/70 lg:bg-transparent p-1.5 rounded-sm hover:bg-stone-700/60`}
         >
           <Image
             className="object-contain w-auto"
@@ -50,7 +50,7 @@ const Taskbar = () => {
         </button>
         <button
           onClick={() => toggleModal("PROJECTS")}
-          className="cursor-pointer w-10 h-10 flex items-center justify-center bg-transparent p-1.5 rounded-sm hover:bg-stone-700/60"
+          className={`cursor-pointer w-10 h-10 flex items-center justify-center bg-black/70 lg:bg-transparent p-1.5 rounded-sm hover:bg-stone-700/60`}
         >
           <Image
             className="object-contain w-auto"
@@ -62,7 +62,7 @@ const Taskbar = () => {
         </button>
         <button
           onClick={() => toggleModal("CONTACT")}
-          className="cursor-pointer w-10 h-10 flex items-center justify-center bg-transparent p-1.5 rounded-sm hover:bg-stone-700/60"
+          className={`cursor-pointer w-10 h-10 flex items-center justify-center bg-black/70 lg:bg-transparent p-1.5 rounded-sm hover:bg-stone-700/60`}
         >
           <Image
             className="object-contain w-auto"
@@ -74,7 +74,7 @@ const Taskbar = () => {
         </button>
       </div>
       {isMobile && (
-        <div className="w-full h-8 flex items-center justify-around bg-transparent">
+        <div className="w-full h-8 flex items-center justify-around bg-[#181818]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -89,20 +89,22 @@ const Taskbar = () => {
               d="M15.75 19.5 8.25 12l7.5-7.5"
             />
           </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-            />
-          </svg>
+          <button onClick={closeAllModals}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          </button>
           <span className="size-4" />
         </div>
       )}
