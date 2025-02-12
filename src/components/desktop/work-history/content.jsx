c0beb894020961fcb8360a803ac91affbb1e165d
AkritiGhosh <akritighosh98@gmail.com>
2025-02-12 12:29:21 +0530

    content separated

---
+++ src/components/desktop/work-history/content.jsx
@@ -0,0 +1,72 @@
+export const techmentPortfolio = [
+  "Built customizable digital fliers and microsites for major universities and hospitals (Stanford, Harvard, UBC) to boost donor engagement. Think of it as a dynamic CMS but tailored for non-profits.",
+  "Created analytics reports using Google Charts to visualize fundraising trends and donor behavior, making reports more insightful.",
+  "Developed a fully responsive, dynamic template CMS that allowed clients to generate webpages with unique URL and download high-quality PDFs of promotional materials.",
+  "Took ownership of the reports and analytics section, improving performance and making data insights more intuitive and accessible.",
+  "Documented onboarding processes for new developers and clients, which became the go-to playbook for every new team member. Conducted code reviews and helped onboard new developers, ensuring clean and maintainable code.",
+  "Worked in a fast-paced Agile environment with daily scrums and quick iterative development based on client feedback.",
+];
+
+export const sproutsPortfolio = [
+  "Built a powerful AI-driven hiring platform from the ground up—think LinkedIn meets an AI-powered ATS that matches candidates to jobs based on resumes and job descriptions.",
+  "Created an interactive job search experience, where candidates could auto-fill profiles via resume parsing, get AI-generated match scores, and apply directly.",
+  "Developed an advanced hiring dashboard for recruiters, integrating real-time analytics, job tracking, and candidate assessments in one place.",
+  "Engineered AI-powered interview assistance, where an AI bot joined interviews, transcribed responses in real-time, and suggested follow-up questions to recruiters.",
+  "Designed data-driven hiring insights using D3.js, giving recruiters clear visual reports on job performance, candidate pipeline, and team efficiency.",
+  "Built a custom component library with Storybook and Tailwind, making UI development faster and more consistent across teams.",
+  "Managed hiring for the engineering team, conducting interviews, onboarding developers, and scaling the team from 4 to 12 members.",
+  "Took on Scrum Master & Project Management responsibilities, leading sprint planning, managing tasks in Jira, and ensuring smooth development cycles.",
+  "Integrated third-party tools like Twilio (for messaging & calls), OpenAI (for AI-powered job descriptions), and Sentry (for error tracking) to enhance the product’s functionality.",
+];
+
+export const techmentImgs = [
+  "techment/techment_logo.jpg",
+  "techment/techment-nextwealth.jpg",
+  "techment/techment_team.jpg",
+  "techment/techment-office.jpg",
+];
+
+export const sproutsFeatureCards = [
+  {
+    id: 0,
+    name: "Manu Arora",
+    designation: "Senior Software Engineer",
+    content: (
+      <p>
+        These cards are amazing, I want to use them in my project. Framer motion
+        is a godsend ngl tbh fam 🙏
+      </p>
+    ),
+  },
+  {
+    id: 1,
+    name: "Elon Musk",
+    designation: "Senior Shitposter",
+    content: (
+      <p>
+        I dont like this Twitter thing, deleting it right away because yolo.
+        Instead, I would like to call it X.com so that it can easily be confused
+        with adult sites.
+      </p>
+    ),
+  },
+  {
+    id: 2,
+    name: "Tyler Durden",
+    designation: "Manager Project Mayhem",
+    content: (
+      <p>
+        The first rule of Fight Club is that you do not talk about fight club.
+        The second rule of Fight club is that you DO NOT TALK about fight club.
+      </p>
+    ),
+  },
+];
+
+export const sharboScreenshots = [
+    "sharbo.jpg",
+    "sharbo_analysis.png",
+    "sharbo_form.png",
+    "sharbo_charts.png",
+  ];
+  
--- src/components/desktop/work-history/page.jsx
+++ src/components/desktop/work-history/page.jsx
@@ -3,71 +3,13 @@ import Image from "next/image";
 import React from "react";
 import { Timeline } from "@/components/ui/timeline";
 import { CardStack } from "@/components/ui/cardStack";
-
-const TechmentPortfolio = [
-  "Built customizable digital fliers and microsites for major universities and hospitals (Stanford, Harvard, UBC) to boost donor engagement. Think of it as a dynamic CMS but tailored for non-profits.",
-  "Created analytics reports using Google Charts to visualize fundraising trends and donor behavior, making reports more insightful.",
-  "Developed a fully responsive, dynamic template CMS that allowed clients to generate webpages with unique URL and download high-quality PDFs of promotional materials.",
-  "Took ownership of the reports and analytics section, improving performance and making data insights more intuitive and accessible.",
-  "Documented onboarding processes for new developers and clients, which became the go-to playbook for every new team member. Conducted code reviews and helped onboard new developers, ensuring clean and maintainable code.",
-  "Worked in a fast-paced Agile environment with daily scrums and quick iterative development based on client feedback.",
-];
-
-const sproutsPortfolio = [
-  "Built a powerful AI-driven hiring platform from the ground up—think LinkedIn meets an AI-powered ATS that matches candidates to jobs based on resumes and job descriptions.",
-  "Created an interactive job search experience, where candidates could auto-fill profiles via resume parsing, get AI-generated match scores, and apply directly.",
-  "Developed an advanced hiring dashboard for recruiters, integrating real-time analytics, job tracking, and candidate assessments in one place.",
-  "Engineered AI-powered interview assistance, where an AI bot joined interviews, transcribed responses in real-time, and suggested follow-up questions to recruiters.",
-  "Designed data-driven hiring insights using D3.js, giving recruiters clear visual reports on job performance, candidate pipeline, and team efficiency.",
-  "Built a custom component library with Storybook and Tailwind, making UI development faster and more consistent across teams.",
-  "Managed hiring for the engineering team, conducting interviews, onboarding developers, and scaling the team from 4 to 12 members.",
-  "Took on Scrum Master & Project Management responsibilities, leading sprint planning, managing tasks in Jira, and ensuring smooth development cycles.",
-  "Integrated third-party tools like Twilio (for messaging & calls), OpenAI (for AI-powered job descriptions), and Sentry (for error tracking) to enhance the product’s functionality.",
-];
-
-const sharboScreenshots = [
-  "sharbo.jpg",
-  "sharbo_analysis.png",
-  "sharbo_form.png",
-  "sharbo_charts.png",
-];
-
-const CARDS = [
-  {
-    id: 0,
-    name: "Manu Arora",
-    designation: "Senior Software Engineer",
-    content: (
-      <p>
-        These cards are amazing, I want to use them in my project. Framer motion
-        is a godsend ngl tbh fam 🙏
-      </p>
-    ),
-  },
-  {
-    id: 1,
-    name: "Elon Musk",
-    designation: "Senior Shitposter",
-    content: (
-      <p>
-        I dont like this Twitter thing, deleting it right away because yolo.
-        Instead, I would like to call it X.com so that it can easily be confused
-        with adult sites.
-      </p>
-    ),
-  },
-  {
-    id: 2,
-    name: "Tyler Durden",
-    designation: "Manager Project Mayhem",
-    content: (
-      <p>
-        The first rule of Fight Club is that you do not talk about fight club.
-        The second rule of Fight club is that you DO NOT TALK about fight club.
-      </p>
-    ),
-  },
-];
+import {
+  sharboScreenshots,
+  sproutsFeatureCards,
+  sproutsPortfolio,
+  techmentImgs,
+  techmentPortfolio,
+} from "./content";
 
 export default function WorkExperience() {
   const data = [
@@ -120,7 +62,7 @@ export default function WorkExperience() {
               Senior Software Engineer - Frontend lead
             </span>
           </p>
-          <CardStack items={CARDS} />
+          <CardStack items={sproutsFeatureCards} />
           <ul className="leading-8 text-base">
             {sproutsPortfolio?.map((pt, ix) => (
               <li key={"spr" + ix}>
@@ -149,12 +91,7 @@ export default function WorkExperience() {
             </span>
           </p>
           <div className="flex flex-col lg:flex-row h-80 w-full relative gap-2">
-            {[
-              "techment/techment_logo.jpg",
-              "techment/techment-nextwealth.jpg",
-              "techment/techment_team.jpg",
-              "techment/techment-office.jpg",
-            ]?.map((url, i) => (
+            {techmentImgs?.map((url, i) => (
               <div
                 key={url}
                 className={`relative min-w-16 w-full lg:w-1/4 hover:w-full h-1/4 lg:h-80 hover:h-full focus:h-full bg-center bg-no-repeat ${
@@ -165,7 +102,7 @@ export default function WorkExperience() {
             ))}
           </div>
           <ul className="text-base mt-5 leading-8">
-            {TechmentPortfolio?.map((portfolio, i) => (
+            {techmentPortfolio?.map((portfolio, i) => (
               <li key={"tp" + i}>
                 <span className="text-coral inline-block mr-2">✫</span>
                 <span>{portfolio}</span>
