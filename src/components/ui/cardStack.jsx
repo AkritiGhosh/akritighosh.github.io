2731a1e98f8aea89653ba6d9ba10a640f5683646
AkritiGhosh <akritighosh98@gmail.com>
2025-02-12 11:57:52 +0530

    light mode removed - default = daek

--- src/components/ui/cardStack.jsx
+++ src/components/ui/cardStack.jsx
@@ -54,7 +54,7 @@ export const CardStack = ({ items, offset, scaleFactor }) => {
         return (
           <motion.div
             key={card.id}
-            className="absolute bg-background h-60 w-60 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1] shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
+            className="absolute bg-background h-60 w-60 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-white/[0.1] shadow-white/[0.05] flex flex-col justify-between"
             style={{
               transformOrigin: "top right",
             }}
@@ -65,14 +65,14 @@ export const CardStack = ({ items, offset, scaleFactor }) => {
               zIndex: cards.length - index, // Decrease z-index for the cards that are behind
             }}
           >
-            <div className="font-normal text-neutral-700 dark:text-neutral-200">
+            <div className="font-normal text-neutral-200">
               {card.content}
             </div>
             <div>
-              <p className="text-neutral-500 font-medium dark:text-white">
+              <p className="font-medium text-white">
                 {card.name}
               </p>
-              <p className="text-neutral-400 font-normal dark:text-neutral-200">
+              <p className="font-normal text-neutral-200">
                 {card.designation}
               </p>
             </div>
--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -23,24 +23,21 @@ export const Timeline = ({ data }) => {
   const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
 
   return (
-    <div
-      className="w-full dark:bg-[#202020] font-sans md:px-10"
-      ref={containerRef}
-    >
+    <div className="w-full bg-[#202020] font-sans md:px-10" ref={containerRef}>
       <div className="max-w-7xl mx-auto pb-5 pt-10 md:pt-16 px-8 md:px-8 lg:px-10 text-center md:text-left">
-        <h1 className="text-5xl md:text-7xl xl:text-9xl text-black dark:text-white max-w-4xl font-display">
+        <h1 className="text-5xl md:text-7xl xl:text-9xl text-white max-w-4xl font-display">
           My experiences so far ...
         </h1>
       </div>
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
         {data.map((item, index) => (
           <div
-            key={"exp"+index}
+            key={"exp" + index}
             className="flex justify-start pt-10 md:pt-40 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-[#202020] flex items-center justify-center">
-                <div className="h-2 w-2 md:h-4 md:w-4 rounded-full bg-teal border border-neutral-300 dark:border-white p-1.5 md:p-2" />
+                <div className="h-2 w-2 md:h-4 md:w-4 rounded-full bg-teal border border-white p-1.5 md:p-2" />
               </div>
               <h3 className="hidden md:inline-block text-xl md:pl-20 md:text-5xl !leading-normal font-bold text-white font-nums">
                 {item.title}
@@ -59,7 +56,7 @@ export const Timeline = ({ data }) => {
           style={{
             height: height + "px",
           }}
-          className="absolute md:left-8 left-[31px] top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          className="absolute md:left-8 left-[31px] top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
           <motion.div
             style={{
