63786fac175ea4a26eb1cc0995b1be5239475c52
AkritiGhosh <akritighosh98@gmail.com>
2025-02-13 19:16:34 +0530

    Project structure

--- src/components/desktop/projects/page.jsx
+++ src/components/desktop/projects/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import useTheme from "@/hooks/useTheme";
+import React from "react";
+
+const fileBarHeightInRems = 2.5;
+const fileBarHeight = "h-" + fileBarHeightInRems * 4;
+const remainingWindowHeight = "h-[calc(100%-" + fileBarHeightInRems + "rem)]";
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+  const { isMobile } = useTheme();
 
-export default Projects
+  return (
+    <div className="w-full h-full bg-[#202020]">
+      <div
+        className={`w-full ${fileBarHeight} flex items-center justify-between gap-4 px-4 bg-[#181818] border-y border-neutral-400/40`}
+      ></div>
+      <div
+        className={`w-full ${remainingWindowHeight} flex items-start`}
+      >
+        {/* Folder Tree */}
+        {!isMobile && (
+          <div className="w-1/4 h-full border-r border-neutral-400/40 overflow-y-auto py-4 px-3"></div>
+        )}
+        {/* Files in current folder */}
+        <div className="w-full h-full overflow-y-auto p-4"></div>
+        {/* Preview/Details pane */}
+        {!isMobile && (
+          <div className="w-1/3 h-full border-l border-neutral-400/40 overflow-y-auto py-4 px-3"></div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
