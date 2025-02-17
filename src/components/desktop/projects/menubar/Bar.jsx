040bede25dce09e0a0e0cebb9e6cbd06b98df2b2
AkritiGhosh <akritighosh98@gmail.com>
2025-02-17 12:13:26 +0530

    menu bar UI

--- src/components/desktop/projects/menubar/Bar.jsx
+++ src/components/desktop/projects/menubar/Bar.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import { ExplorerContext } from "@/context/ExplorerContext";
+import React, { useContext } from "react";
 
 const Bar = () => {
-  return (
-    <div>Bar</div>
-  )
-}
+  const { activeFolder, activeFile } = useContext(ExplorerContext);
+  const folderPath =
+    activeFolder?.title != null ? "/" + activeFolder?.title : "";
+  const filePath =
+    activeFolder?.title != null && activeFile?.name != null
+      ? "/" + activeFile?.name
+      : "";
 
-export default Bar
+  return (
+    <div
+      className={`w-full h-10 flex items-center justify-between gap-4 px-4 py-1 bg-[#181818] border-y border-neutral-400/40`}
+    >
+      <button className="h-full w-auto px-2 py-0 5 rounded-sm text-white text-sm leading-6 min-w-fit hover:bg-neutral-800 group flex items-center gap-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+          />
+        </svg>
+        Sort by
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="size-4 fill-transparent group-hover:fill-white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <button className="h-full w-auto px-2 py-0 5 rounded-sm text-white text-sm leading-6 min-w-fit hover:bg-neutral-800 group flex items-center gap-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
+          />
+        </svg>
+        Group by
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="size-4 fill-transparent group-hover:fill-white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div className="w-full h-full border border-neutral-600 rounded-sm px-2 py-0.5 bg-[#111111]">
+        <p className="text-white text-sm leading-6">
+          {"Akriti Ghosh://Portfolio" + folderPath + filePath}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Bar;
--- src/components/desktop/projects/page.jsx
+++ src/components/desktop/projects/page.jsx
@@ -1,36 +1,31 @@
 import useTheme from "@/hooks/useTheme";
 import React from "react";
 import FileTree from "./FileTree";
-
-const fileBarHeightInRems = 2.5;
-const fileBarHeight = "h-" + fileBarHeightInRems * 4;
-const remainingWindowHeight = "h-[calc(100%-" + fileBarHeightInRems + "rem)]";
+import Bar from "./menubar/Bar";
 
 const Projects = () => {
   const { isMobile } = useTheme();
 
   return (
-      <div className="w-full h-full bg-[#202020]">
-        <div
-          className={`w-full ${fileBarHeight} flex items-center justify-between gap-4 px-4 bg-[#181818] border-y border-neutral-400/40`}
-        ></div>
-        <div className={`w-full ${remainingWindowHeight} flex items-start`}>
-          {/* Folder Tree */}
-          {!isMobile && <FileTree />}
-          {/* Files in current folder */}
-          <div className="w-full h-full overflow-y-auto p-4">
-            {content?.map((data) => (
+    <div className="w-full h-full bg-[#202020]">
+      <Bar />
+      <div className={`w-full h-[calc(100%-2.5rem)] flex items-start`}>
+        {/* Folder Tree */}
+        {!isMobile && <FileTree />}
+        {/* Files in current folder */}
+        <div className="w-full h-full overflow-y-auto p-4">
+          {/* {content?.map((data) => (
               <button key={data?.id} className="flex flex-col p-2 rounded">
                 {data?.name ?? data?.title}{" "}
               </button>
-            ))}
-          </div>
-          {/* Preview/Details pane */}
-          {!isMobile && (
-            <div className="w-1/3 h-full border-l border-neutral-400/40 overflow-y-auto py-4 px-3"></div>
-          )}
+            ))} */}
         </div>
+        {/* Preview/Details pane */}
+        {!isMobile && (
+          <div className="w-1/3 h-full border-l border-neutral-400/40 overflow-y-auto py-4 px-3"></div>
+        )}
       </div>
+    </div>
   );
 };
 
