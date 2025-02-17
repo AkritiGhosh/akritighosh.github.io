3e54b292c2b21f05639e0c33d54192dc1774c7be
AkritiGhosh <akritighosh98@gmail.com>
2025-02-17 16:55:52 +0530

    Main window UI done

---
+++ src/components/desktop/projects/main/MainWindow.jsx
@@ -0,0 +1,36 @@
+import { ExplorerContext } from "@/context/ExplorerContext";
+import Image from "next/image";
+import React, { useContext } from "react";
+
+const MainWindow = () => {
+  const { content, activeFolder } = useContext(ExplorerContext);
+  console.log(content);
+
+  return (
+    <div className="w-full h-full overflow-y-auto p-4 grid grid-cols-2 md:grid-cols-6 lg:grid-cols-4 xl:grid-cols-6 scroll-thin gap-2 md:gap-4">
+      {content?.map((data) => (
+        <button
+          key={data?.id}
+          className="w-full aspect-square flex flex-col items-center gap-y-2 p-2 rounded border border-transparent hover:border-neutral-400/40 hover:bg-neutral-300/20 text-center"
+        >
+          <Image
+            className="object-contain h-[calc(100%-48px)] min-h-16 w-auto"
+            src={
+              activeFolder?.title != null
+                ? "/files/" + data?.fileType + ".png"
+                : "/files/folder.png"
+            }
+            width={112}
+            height={112}
+            alt="Hello"
+          />
+          <span className="w-full">
+            {data?.name ?? data?.title}
+          </span>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default MainWindow;
