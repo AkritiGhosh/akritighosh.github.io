5e90296ca7f8326ccdb08e6241e6862a7b3fd8b7
AkritiGhosh <akritighosh98@gmail.com>
2025-02-18 19:39:07 +0530

    close file for desktop

--- src/components/desktop/projects/preview/PreviewPane.jsx
+++ src/components/desktop/projects/preview/PreviewPane.jsx
@@ -8,8 +8,8 @@ const PreviewPane = () => {
   const { isMobile } = useTheme();
   const { isFileOpen, activeFolder, closeFile } = useContext(ExplorerContext);
   return isFileOpen ? (
-    <div className="w-full h-auto lg:h-full lg:border-l border-neutral-400/40 overflow-y-auto py-2 md:py-4 lg:py-6 px-3 flex flex-col gap-4">
-      {isMobile && (
+    <div className="relative w-full h-auto lg:h-full lg:border-l border-neutral-400/40 overflow-y-auto py-2 md:py-4 lg:py-6 px-3 flex flex-col gap-4 scroll-thin">
+      {isMobile ? (
         <button
           onClick={closeFile}
           className="w-full flex items-center gap-2 text-neutral-400 hover:font-semibold group"
@@ -29,6 +29,23 @@ const PreviewPane = () => {
             />
           </svg>
           <span className="text-sm ">Back to {activeFolder?.title}</span>
+        </button>
+      ) : (
+        <button title="Close preview" className="absolute top-2 right-2 hover:text-red-500" onClick={closeFile}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
         </button>
       )}
       <FilePreview />
