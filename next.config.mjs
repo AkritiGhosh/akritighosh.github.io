e483793430521e7fb6a698bd8569f3a7d5a0f7f5
AkritiGhosh <akritighosh98@gmail.com>
2025-02-19 14:10:55 +0530

    build to pages - part 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,21 @@ const nextConfig = {
   basePath: "",
   output: "export",
   images: {
+    unoptimized: true, // GitHub Pages does not support Next.js image optimization
     remotePatterns: [
       {
         protocol: "https", // Protocol is "https"
         hostname: "assets.aceternity.com", // Exact hostname (do not use wildcards like `**/`)
+        port: "", // Leave port empty for default ports
+        pathname: "/**/**", 
+      },{
+        protocol: "https", // Protocol is "https"
+        hostname: "github.com", // Exact hostname (do not use wildcards like `**/`)
+        port: "", // Leave port empty for default ports
+        pathname: "/**/**", 
+      },{
+        protocol: "https", // Protocol is "https"
+        hostname: "matlabacademy-content.mathworks.com", // Exact hostname (do not use wildcards like `**/`)
         port: "", // Leave port empty for default ports
         pathname: "/**/**", 
       },
